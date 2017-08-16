17d78abde8f464b1420db679f5eeb232a98fc0c2
djurio <djurio@ESAWPNAWS487N4T>
2017-08-16 13:21:13 +0200

    V1

---
+++ GitTestApplication/GitTestApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console test first application version");
+            Console.WriteLine("This is V1");
+            Console.WriteLine("Waiting to read key...");
+            Console.ReadKey();
+        }
+    }
+}
